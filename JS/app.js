80fac034eb24b2ae11d86297d7ce501cb2fc00ac
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-04-08 16:06:21 +0200

    ADD: method in JS to generate 10 emails through the API from Boolean + v-for directive in HTML to print list of emails on the page

--- JS/app.js
+++ JS/app.js
@@ -3,8 +3,21 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        emails: [],
       }
-    }
+    },
+    methods: {
+        getEmails(n) {
+            for (let i = 0; i < n; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.emails.push(response.data.response);
+                })
+            } 
+        }
+    },
+    created(){
+        this.getEmails(10);
+    },
   }).mount('#app')
 
